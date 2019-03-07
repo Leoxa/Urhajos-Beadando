7c87ec91a2e478dd2cc827db324966996657cdc3
Leoxa <32542287+Leoxa@users.noreply.github.com>
2019-03-07 08:07:47 +0100

    Add files via upload

---
+++ meteorSpwn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorSpwn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject meteoritok;
+    [SerializeField]
+    int ismSzam = 5; 
+    void Start()
+    {
+        for (int i = 0; i < ismSzam; i++)
+        {
+            var helyzet = new Vector2(Random.Range(-20, 20), Random.Range(-20, 20));
+            Instantiate(meteoritok, helyzet, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
